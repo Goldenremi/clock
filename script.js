5865d544d7e1ad6eeb974a5f288d655bdf14205a
Boluwatife <oderemiboluwatife@gmail.com>
2022-04-03 18:02:10 +0100

    preetified

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 let hourHand = document.querySelector(".hour-hand");
 let minuteHand = document.querySelector(".minute-hand");
 let secondHand = document.querySelector(".second-hand");
-
+let minuteHands = document.querySelectorAll('[class^="mh"]');
 let hourHands = document.querySelectorAll('[class^="hh"]');
+let clockHands = document.querySelectorAll(".clock-hands>hr");
+
+Array.from(clockHands).map((e, i) => {
+	e.style.backgroundColor = "black";
+	e.style.borderRadius = "5rem";
+});
+
 Array.from(hourHands).map((e, i) => {
 	angle = "rotate(" + i * 30 + "deg)";
 	e.style.transform = angle;
 });
 
-let minuteHands = document.querySelectorAll('[class^="mh"]');
 Array.from(minuteHands).map((e, i) => {
 	angle = "rotate(" + i * 6 + "deg)";
 	i
@@ -18,14 +24,7 @@ Array.from(minuteHands).map((e, i) => {
 		: (e.style.display = "none");
 });
 
-let clockHands = document.querySelectorAll('.clock-hands>hr');
-Array.from(clockHands).map((e, i) => {
-	e.style.backgroundColor='black'
-	e.style.borderRadius= '5rem'	
-});
-
-
-const clockHandsMover = ()=> {
+const clockHandsMover = () => {
 	let now = new Date();
 	let hour = now.getHours();
 	let minute = now.getMinutes();
@@ -35,11 +34,17 @@ const clockHandsMover = ()=> {
 	let minuteText = "deg) translateX( calc(7.5 * var(--smallest_side)))";
 	let secondText = "deg) translateX( calc(9 * var(--smallest_side)))";
 	secondHand.style.transform = leadText + 6 * second + secondText;
-	second? minuteHand.style.transform = leadText + 6 * (minute + second/60) + minuteText:minuteHand.style.transform = leadText + 6 * minute + minuteText;
-	minute? hourHand.style.transform = leadText + 30 * (hour + minute/60) + hourText :hourHand.style.transform = leadText + 30 * hour + hourText;
-}
+	second
+		? (minuteHand.style.transform =
+				leadText + 6 * (minute + second / 60) + minuteText)
+		: (minuteHand.style.transform = leadText + 6 * minute + minuteText);
+	minute
+		? (hourHand.style.transform =
+				leadText + 30 * (hour + minute / 60) + hourText)
+		: (hourHand.style.transform = leadText + 30 * hour + hourText);
+};
 
-clockHandsMover()
+clockHandsMover();
 setInterval(() => {
-	clockHandsMover()
+	clockHandsMover();
 }, 500);
